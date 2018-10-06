599e13ca1711e86b6da4b738e6a63984cba2c822
katejsenger <katejsenger@yahoo.com>
2018-10-06 00:06:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,10 +5,16 @@ class Dog
   def name=(name)
     @name=name
   end
+  def name
+    "{@name}".strip
+  end
   def initialize(breed)
     @breed=breed = "Mutt"
   end
   def breed=(breed)
     @breed=breed 
+  end
+  def breed
+    "{@breed}".strip
   end
 end
